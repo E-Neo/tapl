5da0e6cf31e800ef26f0d28408e6e8ce0a8a8f84
E-Neo <e-neo@qq.com>
2020-12-01 19:51:43 +0800

    update parser to make arith left associative

--- src/arith/parser.rs
+++ src/arith/parser.rs
@@ -1,23 +1,22 @@
 use crate::arith::AstNode;
-use pest::Parser;
+use pest::Parser as PestParser;
 use pest_derive::Parser;
 
 #[derive(Parser)]
 #[grammar = "arith/arith.pest"]
-struct ArithParser;
+struct Parser;
 
 pub fn parse(source: &str) -> Result<AstNode, pest::error::Error<Rule>> {
-    Ok(ArithParser::parse(Rule::Term, source)?
+    Ok(Parser::parse(Rule::Term, source)?
         .next()
         .map(|term| term_to_ast(term))
         .unwrap())
 }
 
 fn term_to_ast(pair: pest::iterators::Pair<Rule>) -> AstNode {
+    let pair = pair.into_inner().next().unwrap();
     match pair.as_rule() {
-        Rule::Term => term_to_ast(pair.into_inner().next().unwrap()),
-        Rule::True => AstNode::True,
-        Rule::False => AstNode::False,
+        Rule::AppTerm => appterm_to_ast(pair),
         Rule::IfThenElse => {
             let mut pair = pair.into_inner();
             let t1 = Box::new(term_to_ast(pair.next().unwrap()));
@@ -25,10 +24,27 @@ fn term_to_ast(pair: pest::iterators::Pair<Rule>) -> AstNode {
             let t3 = Box::new(term_to_ast(pair.next().unwrap()));
             AstNode::IfThenElse(t1, t2, t3)
         }
+        _ => unreachable!(),
+    }
+}
+
+fn appterm_to_ast(pair: pest::iterators::Pair<Rule>) -> AstNode {
+    let pair = pair.into_inner().next().unwrap();
+    match pair.as_rule() {
+        Rule::ATerm => aterm_to_ast(pair),
+        Rule::Succ => AstNode::Succ(Box::new(aterm_to_ast(pair.into_inner().next().unwrap()))),
+        Rule::Pred => AstNode::Pred(Box::new(aterm_to_ast(pair.into_inner().next().unwrap()))),
+        Rule::IsZero => AstNode::IsZero(Box::new(aterm_to_ast(pair.into_inner().next().unwrap()))),
+        _ => unreachable!(),
+    }
+}
+
+fn aterm_to_ast(pair: pest::iterators::Pair<Rule>) -> AstNode {
+    let pair = pair.into_inner().next().unwrap();
+    match pair.as_rule() {
+        Rule::True => AstNode::True,
+        Rule::False => AstNode::False,
         Rule::Zero => AstNode::Zero,
-        Rule::Succ => AstNode::Succ(Box::new(term_to_ast(pair.into_inner().next().unwrap()))),
-        Rule::Pred => AstNode::Pred(Box::new(term_to_ast(pair.into_inner().next().unwrap()))),
-        Rule::IsZero => AstNode::IsZero(Box::new(term_to_ast(pair.into_inner().next().unwrap()))),
-        unknown_term => panic!("unknown term: {:?}", unknown_term),
+        _ => term_to_ast(pair),
     }
 }
